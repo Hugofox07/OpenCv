60b7da6c1996b34234f1718e3b91fa466584cb24
Hugo Leonardo Barbosa <acmhugo@gmail.com>
2025-04-19 20:46:45 -0300

    warpcommit

--- Basic Functions/Warp Perspective .py
+++ Basic Functions/Warp Perspective .py
@@ -1,14 +1,27 @@
 import cv2 as cv
 import numpy as np
 
+# Carregar a imagem
 path = 'imagem/UnoCards.jpg'
-img = cv.imread(path)
-cv.imshow('Imagem', img)
+image = cv.imread(path)
 
+# Dimensão da imagem de saída
+width, height = 300, 400
 
+# Pontos da perspectiva original (manual ou via detecção)
+points_original = np.float32([[669, 229], [797,292], [497, 367], [624, 440]])
 
+# Pontos da visão transformada (destino)
+points_destination = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
 
+# Calcular a matriz de transformação
+matrix = cv.getPerspectiveTransform(points_original, points_destination)
 
+# Aplicar a transformação
+warped_image = cv.warpPerspective(image, matrix, (width, height))
 
+# Exibir os resultados
+cv.imshow("Original", image)
+cv.imshow("Warp Perspective", warped_image)
 cv.waitKey(0)
 cv.destroyAllWindows()

11de8bc5545f940dd59b96a87edb85784ceeafb8
Hugo Leonardo Barbosa <acmhugo@gmail.com>
2025-04-21 20:30:32 -0300

    cascade

--- Haar Cascade Object Detection/Detecção-Objetos.py
+++ Haar Cascade Object Detection/Detecção-Objetos.py
@@ -1,10 +1,19 @@
 import cv2 as cv 
 import numpy as np
 
-camera = cv.VideoCapture(0)
+cap = cv.VideoCapture(0)  # 0 para webcam
 
 while True:
-    check,img = camera.read()
-cv.imshow('Imagem', img)   
-cv.waitKey(1) 
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('Vídeo em Escala de Cinza', gray)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv.imshow('Imagem', cap)   
+cap.realese() 
 cv.destroyAllWindows()

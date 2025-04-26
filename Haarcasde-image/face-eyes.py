1d105d073e4e1ff5c8b0ccd155754cac9fbfb090
Hugo Leonardo Barbosa <acmhugo@gmail.com>
2025-04-26 15:36:11 -0300

    web

--- Haarcasde-image/face-eyes.py
+++ Haarcasde-image/face-eyes.py
@@ -7,7 +7,8 @@ eye_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_eye.xml')
 
 # Ler a imagem (substitua pelo caminho da sua imagem ou use a webcam)
 path = 'imagem/elon.jpeg'
-image = cv.imread(path)
+image = cv.imread(path) 
+
 # Converter a imagem para escala de cinza
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  
 
@@ -15,15 +16,15 @@ gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
 for (x, y, w, h) in faces:
-    # Desenhar um retângulo ao redor do rosto
+# Desenhar um retângulo ao redor do rosto
     cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
     roi_gray = gray[y:y + h, x:x + w]
     roi_color = image[y:y + h, x:x + w]
 
-    # Detectar olhos dentro da região do rosto
+# Detectar olhos dentro da região do rosto
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex, ey, ew, eh) in eyes:
-        cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+     cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
 # Mostrar a imagem com detecções
 cv.imshow('Deteccao de Olhos', image)

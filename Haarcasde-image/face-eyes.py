7c8174f8ca3420383244b0bd3b94f07c464e1780
Hugo Leonardo Barbosa <acmhugo@gmail.com>
2025-04-26 22:48:19 -0300

    faces-eyes

--- Haarcasde-image/face-eyes.py
+++ Haarcasde-image/face-eyes.py
@@ -14,17 +14,16 @@ gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 # Detectar rostos
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
 for (x, y, w, h) in faces:
-# Desenhar um retângulo ao redor do rosto
+# Desenhar um retângulo aoredor do rosto
     cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
     roi_gray = gray[y:y + h, x:x + w]
     roi_color = image[y:y + h, x:x + w]
 
 # Detectar olhos dentro da região do rosto
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in eyes:
-     cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+eyes = eye_cascade.detectMultiScale(roi_gray)
+for (ex, ey, ew, eh) in eyes:
+    cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
 # Mostrar a imagem com detecções
 cv.imshow('Deteccao de Olhos', image)
